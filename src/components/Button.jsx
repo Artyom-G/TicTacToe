847221ab595385f9d45a388ebea8868018f6b24a
Artyom WSL <artyomgv05@gmail.com>
2025-01-11 00:19:12 -0500

    styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import "./Button.css";
 
 const Button = ({ symbol, row, column, makeMove, isWinningCell }) => {
     return(
-        <button className={`grid-button ${symbol!==" " ? `grid-button-${symbol}` : ''} grid-button-${symbol}-${isWinningCell}`}  onClick={() => { makeMove(row, column); }}>
+        <button className={`grid-button ${symbol!==" " && !isWinningCell ? `grid-button-${symbol}` : ''} grid-button-${symbol}-${isWinningCell}`}  onClick={() => { makeMove(row, column); }}>
             <span>{symbol}</span>
         </button>
     );
