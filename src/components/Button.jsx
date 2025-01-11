83bd1ca258bff2c3c46118429446fd996816cd2d
Artyom WSL <artyomgv05@gmail.com>
2025-01-11 00:38:51 -0500

    final changes to code

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import "./Button.css";
 
 const Button = ({ symbol, row, column, makeMove, isWinningCell }) => {
     return(
-        <button className={`grid-button ${symbol!==" " && !isWinningCell ? `grid-button-${symbol}` : 'grid-button-empty'} grid-button-${symbol}-${isWinningCell}`}  onClick={() => { makeMove(row, column); }}>
+        <button className={`grid-button ${symbol!==" " && !isWinningCell ? `grid-button-${symbol}` : 'grid-button-empty'} grid-button-${symbol}-${isWinningCell}`}  onClick={() => { makeMove(row, column); }} disabled={isWinningCell}>
             <span>{symbol}</span>
         </button>
     );
